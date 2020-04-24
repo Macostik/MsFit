2fef1b9ad905ecfc18869ea29da457d15704ed16
Macostik <y.granchenko@gmail.com>
2020-04-24 12:45:48 +0300

    add Int+Ext on timer

--- Ms Fit/Extensions/Int+Ext.swift
+++ Ms Fit/Extensions/Int+Ext.swift
@@ -18,4 +18,12 @@ extension Int {
             return "مرة"
         }
     }
+    
+    func minutesToTime() -> String {
+        let (_,m,_) = (self / 3600, (self % 3600) / 60, (self % 3600) % 60)
+        //        let h_string = h < 10 ? "0\(h)" : "\(h)"
+        let m_string =  m < 10 ? "0\(m)" : "\(m)"
+        //    let s_string =  s < 10 ? "0\(s)" : "\(s)"
+        return "\(m):\(m_string)"
+    }
 }
